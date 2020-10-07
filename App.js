dda4d637db7c4bda8336c0eb6fc154322d3b6f49
jay311311 <jay311311@gmail.com>
2020-10-08 01:37:11 +0900

    making a temp props with weatherAPI

--- App.js
+++ App.js
@@ -1,9 +1,43 @@
 import React from 'react';
 import Loader from "./Loader"
+import * as Location from 'expo-location';
+import { Alert } from 'react-native';
+import axios from "axios"
+import Weather from './Weather';
 
-export default function App() {
-  return (
-    <Loader/>
-  );
+const api = "e71e68187e6da07eb17db48a8cf2dc1b"
+
+export default class extends React.Component {
+  state={
+    isLoading:true,
+
+  }
+
+  getweather = async(latitude,longitude) =>{
+    const {data} = await axios.get(`http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${api}&units=metric`)
+  console.log(data)
+  this.setState({isLoading:false, temp:data.main.temp})
+  }
+  getlocation = async() => {
+    try{
+     
+      await Location.requestPermissionsAsync()
+      const {coords:{latitude,longitude}} = await Location.getCurrentPositionAsync()
+      this.getweather(latitude,longitude)
+      console.log(latitude,longitude)
+    } catch(error){
+      Alert.alert("this is error")
+    }
+  }
+  componentDidMount(){
+  this.getlocation()
+}
+
+  render(){
+    const {isLoading,temp} = this.state
+    return(
+      isLoading ? <Loader/> : <Weather temp={Math.round(temp)}/>
+    )
+  }
 }
 
